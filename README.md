f8f9c9a3b1aa7938eebdea3dfc39d91b46e50184
Shyju Krishnankutty <connectshyju@gmail.com>
2021-04-04 16:23:48 -0700

    Printing the summary(response code count) after the run

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # go-load
  
-A simple load testing tool using go
+A simple load testing tool using go. 
+
+There are tons of go load testing tools out there. This was something I built as I was learning go.
 
 ### Usage exmaple
 
 
 ### Send 100 requests bing.com
-    go-load -rc=100 "https://www.bing.com"
+    go-load -rc=100 https://www.bing.com
+
+If you have special characters like `&` in your URL, you need to pass the value in quotes.
+
+    go-load -rc 100  "https://www.bing.com?how=are&you=today"
+
+### Send 100 requests bing.com with request headers
+Request headers can be passed as a comma separated string with the `-h` flag. The string should have header key and value in the `key:value` format. Ex: `User-Agent:Go-http-client/1.1` 
+
+    go-load -h my-apikey:foo,cookie:uid:bar https://www.bing.com
+
+The above example is sending 2 request headers, "my-apikey" and "cookie".
 
 ### Command line options
 * -rc : Specifies the request count. Ex: `-rc=1000` will issue 1000 requests
+* -h : Specifies the the request headers to send in a comma separated form. Each header item can use the `key:value` format. Ex: `my-apikey:foo,cookie:uid`
+* -u : Specifies the the request URL explicitly.
--- go-load.go
+++ go-load.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -13,7 +14,8 @@ import (
 func main() {
 
 	urlPtr := flag.String("u", "", "The URL to test")
-	requestCountPtr := flag.Int("rc", 10, "Total number of requests to send")
+	requestCountPtr := flag.Int("rc", 1, "Total number of requests to send")
+	headersPtr := flag.String("h", "", "The request headers in comma separated form")
 
 	flag.Parse()
 
@@ -27,37 +29,64 @@ func main() {
 			os.Exit(3)
 		}
 	}
+	var headerString = *headersPtr
+	var maxRequestCount = *requestCountPtr
 
 	var wg sync.WaitGroup
 
-	var maxRequestCount = *requestCountPtr
-
 	fmt.Printf("📢 Will send %d requests to %s \n", maxRequestCount, url)
 
+	client := &http.Client{}
+	responseStatusCountMap := make(map[string]int)
+
 	start := time.Now()
+
 	// do a warmup call
 	wg.Add(1)
-	makeRestCallAsync(url, &wg)
+	makeRestCallAsync(client, url, headerString, &wg, responseStatusCountMap)
 
 	for counter := 1; counter < maxRequestCount; counter++ {
 		wg.Add(1)
-		go makeRestCallAsync(url, &wg)
+		go makeRestCallAsync(client, url, headerString, &wg, responseStatusCountMap)
 	}
 
 	wg.Wait()
 	end := time.Now()
 	elapsed := end.Sub(start)
 
-	fmt.Printf("✨ Total Elapsed time %s", elapsed)
+	fmt.Println("✨ Response codes received(count)")
+	for k, v := range responseStatusCountMap {
+		fmt.Printf("       %s: %d\n", k, v)
+	}
+	fmt.Printf("🎉 Total Elapsed time %s \n", elapsed)
 }
 
-func makeRestCallAsync(url string, wg *sync.WaitGroup) {
+func makeRestCallAsync(client *http.Client, url string, headersCommaSeparated string, wg *sync.WaitGroup, responseStatusCountMap map[string]int) {
 	start := time.Now()
-	var resp, httpCallError = http.Get(url)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	allHeaders := strings.Split(headersCommaSeparated, ",")
+
+	for _, header := range allHeaders {
+		headerStringNameAndValueArray := strings.Split(header, ":")
+		if len(headerStringNameAndValueArray) == 2 {
+			req.Header.Set(headerStringNameAndValueArray[0], headerStringNameAndValueArray[1])
+		}
+	}
+
+	var resp, httpCallError = client.Do(req)
 	if httpCallError == nil {
 		end := time.Now()
 		elapsed := end.Sub(start)
 		fmt.Printf("%s %s Elapsed: %s \n", url, resp.Status, elapsed)
+
+		val, keyPresent := responseStatusCountMap[resp.Status]
+
+		if keyPresent {
+			responseStatusCountMap[resp.Status] = val + 1
+		} else {
+			responseStatusCountMap[resp.Status] = 1
+		}
 
 		wg.Done()
 	} else {
